474e35e8b39dcbec14f2b02ce92e5e32d494cb70
zinzinthin <zinzinthinzaw@gmail.com>
2025-02-26 14:35:33 +0630

    datatypes

---
+++ part1_basic/l3datatypes/dist/l3datatypes.js
@@ -0,0 +1,41 @@
+"use strict";
+let fullname = "Su Su";
+console.log(fullname);
+let age = 25;
+console.log(age);
+let hascar = true;
+console.log(hascar);
+hascar = false;
+console.log(hascar);
+let studentid;
+studentid = 1001;
+studentid = "1002";
+let colorsarrs = ["red", "green", "blue"];
+console.log(colorsarrs);
+let numarrs = [10, 20, 30, 40, 50];
+console.log(numarrs);
+let mixarrs1 = [100, "red", "green", "blue", 200, 300];
+console.log(mixarrs1);
+let maxarrs2 = [true, 100, "red", false];
+console.log(maxarrs2);
+let evenarrs = [
+    [3, 4],
+    [5, 6, 30, 23],
+    [0, 6, 3]
+];
+console.log(evenarrs);
+let greetarrs = [
+    ["hi"],
+    ["hello", "hay"],
+    ["hifi', 'zoho"]
+];
+console.log(greetarrs);
+let infoarrs = [
+    ["aung aung", 100],
+    ["age", 28],
+    ['hobby', 'sport', 'reading'],
+    ['hascar', false]
+];
+console.log(infoarrs);
+const product = ["Redbull", 2500];
+console.log(product);
